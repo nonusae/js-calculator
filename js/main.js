fef0e996e052fe9609b36bf5013555f5261b2243
Natthakit <nonusae@gmail.com>
2018-03-26 00:21:07 +0700

    Finish Up calculator with serveral edge case fixed

--- js/main.js
+++ js/main.js
@@ -26,10 +26,15 @@ keys.addEventListener('click', e => {
     const displayNumber = display.textContent
     const previousKeyType = calculator.dataset.previousKeyType;
 
+    // Number Keys
     if (!action){
       calculator.dataset.previousKeyType = 'number';
 
-      if (displayNumber === '0' || previousKeyType === 'operator') {
+      if (
+        displayNumber === '0' ||
+        previousKeyType === 'operator' ||
+        previousKeyType === 'calculate'
+      ) {
         display.textContent = keyContent;
       } else {
         display.textContent = displayNumber + keyContent;
@@ -39,36 +44,82 @@ keys.addEventListener('click', e => {
         .forEach(k => k.classList.remove('is-depressed'));
     }
 
+    if (action !== 'clear') {
+      const clearButton = calculator.querySelector('[data-action=clear]');
+      clearButton.textContent = 'CE';
+    }
     if (
       action === 'add' ||
       action === 'subtract' ||
       action === 'multiply' ||
       action === 'divide'
     ) {
-      key.classList.add('is-depressed')
-      calculator.dataset.previousKeyType = 'operator';
-      calculator.dataset.firstValue = displayNumber;
-      calculator.dataset.operator = action
-
-    }
-
-    if (action === 'decimal'){
-      display.textContent = displayNumber + '.'
-      calculator.dataset.previousKeyType = 'decimal';
-    }
-
-    if (action === 'clear'){
-      console.log('clear key!')
-      calculator.dataset.previousKeyType = 'clear';
-    }
-
-    if (action === 'calculate'){
-      calculator.dataset.previousKeyType = 'equal';
+      if ( previousKeyType === 'operator') {
+      Array.from(key.parentNode.children)
+        .forEach(k => k.classList.remove('is-depressed'));
+      }
       const firstValue = calculator.dataset.firstValue
       const operator = calculator.dataset.operator
       const secondValue = displayNumber
 
-      display.textContent = calculate(firstValue, operator, secondValue)
+      if (firstValue &&
+          operator &&
+          previousKeyType !== 'operator' &&
+          previousKeyType !== 'calculate') {
+        const calcValue = calculate(firstValue, operator, secondValue);
+        display.textContent = calcValue;
+
+        // Update calated value as firstValue
+        calculator.dataset.firstValue = calcValue;
+      } else {
+        // If there are no calcualtions, set displayNumber as the firstValue
+        calculator.dataset.firstValue = displayNumber;
+      }
+
+      key.classList.add('is-depressed');
+      calculator.dataset.previousKeyType = 'operator';
+      calculator.dataset.operator = action
+    }
+
+    if (action === 'decimal'){
+      if (previousKeyType === 'operator') {
+        display.textContent = '0.';
+      } else if (!displayNumber.includes('.')) {
+        display.textContent = displayNumber + '.';
+      }
+
+      calculator.dataset.previousKeyType = 'decimal';
+    }
+
+    if (action === 'clear'){
+      if (key.textContent === 'AC'){
+        calculator.dataset.firstValue = ''
+        calculator.dataset.modValue = ''
+        calculator.dataset.operator = ''
+        calculator.dataset.previousKeyType = ''
+      } else {
+        key.textContent = 'AC';
+      }
+      display.textContent = '0'
+      calculator.dataset.previousKeyType = 'clear';
+    }
+
+    if (action === 'calculate'){
+      let firstValue = calculator.dataset.firstValue
+      let secondValue = displayNumber
+      const operator = calculator.dataset.operator
+
+      if (firstValue){
+        if (previousKeyType === "calculate") {
+          firstValue = displayNumber
+          secondValue = calculator.dataset.modValue
+        }
+
+        display.textContent = calculate(firstValue, operator, secondValue);
+      }
+
+      calculator.dataset.modValue = secondValue
+      calculator.dataset.previousKeyType = 'calculate';
     }
 
   }
